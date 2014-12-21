32372ba3b35a8c2b80e9f2fb7151866374531d22
lszperling <lszperling@gmail.com>
2014-12-21 22:51:27 +0100

    Starter stuff

---
+++ run_analysis.R
@@ -0,0 +1,28 @@
+library(dplyr)
+x_test <- read.table("test//X_test.txt")
+x_train <- read.table("train//X_train.txt")
+x_merge <- rbind(x_test,x_train)
+y_test <- read.table("test//Y_test.txt")
+y_train <- read.table("train//Y_train.txt")
+y_merge <- rbind(y_test,y_train)
+colnames(y_merge) <- "Activity"
+
+subject_test <- read.table("test//subject_test.txt")
+subject_train <- read.table("train//subject_train.txt")
+subject_merge <- rbind(subject_test, subject_train)
+colnames(subject_merge) <- "Subject"
+
+features <- read.table("features.txt")
+colnames(x_merge) <- features$V2
+filtered <- x_merge[ , grepl( "(mean)|(std)" , names( x_merge ), ignore.case = TRUE ) ]
+complete <- cbind(filtered,y_merge)
+
+activities <- read.table("activity_labels.txt")
+appropiate <- within(complete, Activity <- factor(Activity, labels = activities$V2))
+full <- cbind(appropiate,subject_merge)
+
+to_filter <- tbl_df(full)
+grouped  <- group_by(full, Activity, Subject)
+summarised <- summarise_each(grouped, funs(mean))
+write.table(summarised, "tidy data.txt", sep = ",", row.names = FALSE)
+
